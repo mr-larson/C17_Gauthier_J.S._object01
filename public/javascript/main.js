6d662b26acfddef942ad6828949cda7b0f797b0a
GAUTHIER <gauthierdewit@gmail.com>
2021-02-03 14:47:25 +0100

    debut exo 02

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -7,52 +7,75 @@ let boolean = true || false
 let tableau = ["Hello", 5, false, ["Helloo", 55, true]]
 console.log (typeof tableau)
  */
-// EXO 1
+// EXO_01
 //-----------------------
 //1.1
-let a = {
+/* let a = {
     nom: "Skywalker",
     prenom: "Luke",
     age: 20,
     taille: 170,
 }
-console.log (a .age)
+console.log (a .age) */
 //-----------------------
 //1.2
-let b = {
+/* let b = {
     nom: "San",
     prenom: "Goku",
     age: 30,
     taille: 180,
-}
+} */
+/* let c = {
 
+} */
 //----------------------
 //1.3
-let c = {
+/* let c = {
     nom: a.nom,
     age: b.age,
     taille: 185,
 }
+
 console.log (c)
+
+//bis
+c.nom = a.nom
+c.age = a.age
+c.taille = 185
+
+console.log (c) */
 //-----------------------
 //1.4
-let d = {
+/* let d = {
     saluer(){
         console.log (`coucou ${b.nom}`)
     }
 }
-d.saluer()
+d.saluer() */
 //----------------------
 //1.5
-let e = {
+/* let e = {
     nom: "Maclay",
     prenom: "John",
     age: 40,
     taille: 175,
 }
 let voleur ={
-    
+    usurpation(personne){
+        this.nom = personne.nom
+        this.age = presonne.age
+    }
 }
-voleur = (`Le nom est ${e.nom} \n et l'age est ${a.age} ans`)
+voleur.usurpation(b)
+
 console.log (voleur)
+ */
 //------------------------
+//EXO_02
+//2.1
+let a = {
+    nom: "Skywalker",
+    prenom: "Luke",
+    age: 20,
+    taille: 170,
+}
